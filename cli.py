37435ed94001988594648dbbefdbe6ef93390ad8
Nicolò Toscano <nicolo.ax@gmail.com>
2023-05-07 14:02:17 +0200

    minor changes

--- cli.py
+++ cli.py
@@ -2,13 +2,13 @@ import argparse
 from main import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-op', '--operation', type=str, help='initial operation')
-parser.add_argument('-step', '--step', type=str, help='last input from user')
-parser.add_argument('-task', '--task', default='expand', type=str, help='extra input from exercise instructions')
-parser.add_argument('-last', '--last_correct', type=str, help='last correct step')
+parser.add_argument('-o', '--original', type=str, help='initial operation')
+parser.add_argument('-s', '--step', type=str, help='last input from user')
+parser.add_argument('-t', '--task', default='expand', type=str, help='extra input from exercise instructions')
+parser.add_argument('-l', '--last_correct', type=str, help='last correct step')
 args = parser.parse_args()
 
-operation = args.operation
+operation = args.original
 step = args.step
 task = args.task
 last_correct = args.last_correct
